1ca78f734c935900068264402f6df8f673eee616
juanmunoz22-bit <63082861+juanmunoz22-bit@users.noreply.github.com>
2021-10-09 17:49:38 -0500

    Update app.py
    Endpoint para obtener mascotas con rangos deficientes de signos vitales

--- app.py
+++ app.py
@@ -20,6 +20,28 @@ def record(petId):
     col.insert_one(record)
     return "Se agrego un nuevo registro para la mascota {petId}".format(petId = petId), 201
 
+@app.route("/pet/vitals")
+def vitals_out_of_range():
+    m=get_mongo_conn()
+    db = m["taller3"]
+    col = db["pets"]
+    petId = int(request.args.get('petId'))
+    timestamp1 =  request.args.get('date1')
+    timestamp2 =  request.args.get('date2')
+    cursor = col.find({"petId": petId, "datetime": {"$in":[timestamp1, timestamp2]}})
+    if cursor is not None:
+        cursor_temp = col.find({"temperature": {"$not": {"$in": [38.3, 39.2]}}}, {"_id": 0})
+        '''cursor_hr = col.find({"heart-rate": {"$not": {"$in": [60, 120]}}}, {"_id": 0})
+        cursor_breath = col.find({"breathing-frecuency": {"$not": {"$in": [10, 30]}}}, {"_id": 0})'''
+        pets = []
+        for pet in cursor_temp:
+            pets.append(pet)
+        return { "result": pets }
+    else:
+        return "No existe esta mascota en el registro"
+
+
+
 if __name__ == "__main__":
     # run() method of Flask class runs the application
     # on the local development server.
